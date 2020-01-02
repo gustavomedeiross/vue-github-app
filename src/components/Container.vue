3d16297c3c8daddd16f58586c4a3a1e51fd5d0a9
Gustavo <gustavomendes.medeiros@gmail.com>
2020-01-01 23:10:29 -0300

    Create Container component

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.container {
+  max-width: 700px;
+  margin: 80px auto 0;
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 30px;
+
+  h2 {
+    svg {
+      margin-right: 10px;
+    }
+  }
+}
+</style>
