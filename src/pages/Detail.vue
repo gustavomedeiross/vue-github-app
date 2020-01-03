1847ad6c32ed4469bd16b6a1dbd16b7a67549dec
Gustavo <gustavomendes.medeiros@gmail.com>
2020-01-02 21:09:50 -0300

    2- Create Detail page

---
+++ src/pages/Detail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-link to="/">Go Back to Repositories</router-link>
+    <div class="repository-info">Repository Info</div>
+    <div class="issues-wrapper">
+      Issues
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {},
+
+  created() {},
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
